ef731c77326e827da24555fb3535836ca01d77f0
JMigneault <jacobrmigneault@gmail.com>
2023-12-15 14:00:31 -0500

    Add wiggle.

--- Assets/Scripts/GridEntity.cs
+++ Assets/Scripts/GridEntity.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using System.Random;
 
 public class GridEntity : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class GridEntity : MonoBehaviour
 
   public Sprite litBulb;
 
+  public bool wobblingLeft = false;
+  public float currentWobble;
+  public float wobbleDegrees = 100.0f;
+  public float wobbleSpeed = 80.0f;
+
   public void SetCoords(GridCoords coords, float speed) {
     this.coords = coords;
     StartCoroutine(MoveTo(coords, speed));
@@ -21,10 +27,39 @@ public class GridEntity : MonoBehaviour
 
   public IEnumerator MoveTo(GridCoords coords, float speed) {
     Vector3 target = GridBoard.instance.GetLocalPos(coords);
+    bool shouldWobble = speed < Mathf.Infinity;
+
+    // Start wobbling
+    if (shouldWobble) {
+      wobblingLeft = Random.value > 0.5f;
+      currentWobble = (2 * Random.value - 1) * wobbleDegrees;
+      transform.Rotate(0, 0, currentWobble);
+    }
 
     while ((target - transform.localPosition).magnitude > Mathf.Epsilon) {
       transform.localPosition = Vector3.MoveTowards(transform.localPosition, target, speed * Time.deltaTime);
+      // Wobble
+      if (shouldWobble) {
+        // keep wobbling!
+        float dir = wobblingLeft ? 1 : -1 ;
+        if (dir * currentWobble < wobbleDegrees) {
+          float dWobble = dir * wobbleSpeed * Time.deltaTime;
+          transform.Rotate(0, 0, dWobble);
+          currentWobble += dWobble;
+        } else {
+          // reverse the wobble!
+          wobblingLeft = !wobblingLeft;
+        }
+      }
+
+
       yield return null;
     }
+
+    // Reset wobble
+    if (shouldWobble) {
+      transform.rotation = Quaternion.identity;
+    }
+    yield return null;
   }
 }
